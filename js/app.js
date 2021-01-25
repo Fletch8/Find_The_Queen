f0f3b9122af0213bddb18924784c853e9baa070d
Fletch S <stampsfj04@gmail.com>
2021-01-25 14:47:12 -0500

    added pseudo code

---
+++ js/app.js
@@ -0,0 +1,11 @@
+//document.querySelector('h1').innerText = "test test test"
+
+// make start div with start button 
+
+// add event listener to button
+// when clicked call function to populate screen with cards face dowm
+
+// add click event listeners to each card 
+
+// when clicked if the card is the queen Game is over player wins, then play win animation, ask player to play again
+// else take 1 life, check if game is over
