407eab44540281ab2b737f5101f3e8cf53171055
Fletch S <stampsfj04@gmail.com>
2021-01-26 14:06:54 -0500

    js populates screen with cards

--- js/app.js
+++ js/app.js
@@ -1,5 +1,3 @@
-//document.querySelector('h1').innerText = "test test test"
-
 // make start div with start button 
 
 // add event listener to button
@@ -7,5 +5,17 @@
 
 // add click event listeners to each card 
 
-// when clicked if the card is the queen Game is over player wins, then play win animation, ask player to play again
+// when  clicked if the card is the queen Game is over player wins, then play win animation, ask player to play again
 // else take 1 life, check if game is over
+
+let bodyDiv = document.querySelector('div')
+for(let i = 0; i < 28; i++){
+    let cardDiv = document.createElement('div')    
+    cardDiv.innerHTML = '<img src="assets/queenOfHearts.png"/>'
+    cardDiv.style.width = '100px'
+    cardDiv.style.height = '200px'
+    cardDiv.addEventListener('click', () => {
+        console.log("Found the queen")
+    })
+    bodyDiv.appendChild(cardDiv)
+}
