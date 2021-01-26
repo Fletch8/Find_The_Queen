4a4330969ecbbe94bce2a5436f107bc6497574cd
Fletch S <stampsfj04@gmail.com>
2021-01-26 17:49:26 -0500

    added functionality to check if queen was clicked

--- js/app.js
+++ js/app.js
@@ -1,21 +1,35 @@
-// make start div with start button 
+// all card divs get appended to the main div
+let mainDiv = document.querySelector('div')
+const makeCards = () => {
+    
+    // makes x amount of cards
+    for(let i = 0; i < 28; i++){
+        
+        // select random card image
+        let randNum = Math.floor(Math.random() * 9) + 2
+        let inHTML = `<img src="assets/${randNum}-OfHearts.png"/>` 
+        let cardDiv = document.createElement('div')    
+        cardDiv.innerHTML = inHTML
+        cardDiv.style.width = '100px'
+        cardDiv.style.height = '200px'
 
-// add event listener to button
-// when clicked call function to populate screen with cards face dowm
+        // add event listener to check if queen was found // need to fix src check for hosting on different servers
+        cardDiv.addEventListener('click', (e) => {          
 
-// add click event listeners to each card 
+            if (String(e.target.src) == 'http://127.0.0.1:5500/assets/queenOfHearts.png'){
+                console.log('You Found The QUEEN!!!')
+            }else{
+                console.log(e.target.src)
+            }
+        })
+        cardDiv.setAttribute('id', randNum)
+        mainDiv.appendChild(cardDiv)
+    }
 
-// when  clicked if the card is the queen Game is over player wins, then play win animation, ask player to play again
-// else take 1 life, check if game is over
-
-let bodyDiv = document.querySelector('div')
-for(let i = 0; i < 28; i++){
-    let cardDiv = document.createElement('div')    
-    cardDiv.innerHTML = '<img src="assets/queenOfHearts.png"/>'
-    cardDiv.style.width = '100px'
-    cardDiv.style.height = '200px'
-    cardDiv.addEventListener('click', () => {
-        console.log("Found the queen")
-    })
-    bodyDiv.appendChild(cardDiv)
+    // randomly appends queen card 
+    let rand = Math.floor(Math.random() * mainDiv.children.length)
+    mainDiv.children[rand].innerHTML = `<img src="assets/queenOfHearts.png"/>`
+    mainDiv.children[rand].setAttribute('id', 'queen')
+    console.log(mainDiv.children[rand])
 }
+makeCards()
