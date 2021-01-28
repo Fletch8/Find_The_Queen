a1e060958036bf613b45299433f8908c251e2291
Fletch S <stampsfj04@gmail.com>
2021-01-27 19:03:10 -0500

    added win/lose functionality

--- js/app.js
+++ js/app.js
@@ -1,11 +1,25 @@
 // all card divs get appended to the main div
+let startButton = document.querySelectorAll('button')[0]
+let resetButton = document.querySelectorAll('button')[1]
 let mainDiv = document.querySelector('div')
 let livesH1 = document.getElementById('lives')
 let lives = 4
-const makeCards = () => {
-    
+let numOfCards = 9
+
+// used for setting up next round, removes all elements then remakes them
+const removeCard = () =>{
+    mainDiv.innerHTML = ''
+    makeCards(numOfCards)
+}
+
+// used when game is over
+const reset = () => {
+    location.reload()
+}
+
+const makeCards = (numOfCards) => {    
     // makes x amount of cards
-    for(let i = 0; i < 9; i++){
+    for(let i = 0; i < numOfCards; i++){
         
         // selects random card image
         let randNum = Math.floor(Math.random() * 9) + 2
@@ -15,22 +29,34 @@ const makeCards = () => {
         cardDiv.style.width = '100px'
         cardDiv.style.height = '200px'
 
-        // add event listener to check if queen was found 
+        // add event listener to check if queen was found // fix src for diffent host machines
         cardDiv.addEventListener('click', (e) => {          
             let id = e.target.id
-            // need to fix src check for hosting on different servers
+            // checks element ID for 'queen'
             if (id == 'queen'){
                 console.log('You Found The QUEEN!!!')
                 e.target.src = `http://127.0.0.1:5500/assets/queenOfHearts.png`
+                // just restarts game right now, eventually will start next round after setTimeout()
+                if(confirm('You found the Queen!! play again')){
+                    reset()
+                }else{
+                    reset()
+                }
             }else{
                 lives = lives - 1
-                livesH1.innerText = `Lives: ${lives}`
-                console.log(id)
+                livesH1.innerText = `Lives: ${lives}`               
                 e.target.src = `http://127.0.0.1:5500/assets/${id}-OfHearts.png`
+                if(lives == 0){
+                    if(confirm('Oh no! you are out of lives play again')){
+                        reset()
+                    }else{
+                        reset()
+                    }
+                }
             }
         })
 
-        // 'ID' is prob only needed for the queen
+        // set cardDiv ID's for tracking cards
         cardDiv.setAttribute('id', randNum)
         mainDiv.appendChild(cardDiv)
     }
@@ -42,9 +68,17 @@ const makeCards = () => {
     console.log(mainDiv.children[rand])
 }
 
-makeCards()
+startButton.addEventListener('click', ()=>{
+    if(mainDiv.children.length == 0){
+        makeCards(numOfCards)    
+    }    
+})
+resetButton.addEventListener('click', ()=>{
+    reset()
+})
 
 
 
-// https://upload.wikimedia.org/wikipedia/commons/5/55/Atlasnye_playing_cards_deck.svg
+// card front side images from  https://upload.wikimedia.org/wikipedia/commons/5/55/Atlasnye_playing_cards_deck.svg
+// card backside from https://www.pinterest.com/pin/105130972527979556/
 // computers are like air conditioning, as soon as you open up windows it becomes useless 
