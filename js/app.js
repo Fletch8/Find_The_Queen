6bf885159bc55b6ab143035be2b290bb1af81e47
Fletch S <stampsfj04@gmail.com>
2021-01-28 19:13:54 -0500

    added card flip animation

--- js/app.js
+++ js/app.js
@@ -17,21 +17,32 @@ const reset = () => {
     location.reload()
 }
 
+
 const makeCards = (numOfCards) => {    
     // makes x amount of cards
     for(let i = 0; i < numOfCards; i++){
         
         // selects random card image
         let randNum = Math.floor(Math.random() * 9) + 2
-        let inHTML = `<img id="${randNum}"src="assets/cardBackSide.png"/>` 
+        let inHTML = `<div id='${randNum} 'class="flip-card">
+                            <div id='flip-card-inner-${randNum}'class="flip-card-inner">
+                                <div class="flip-card-front">
+                                    <img src="assets/cardBackSide.png" style="width:100px;height:200px;">
+                                </div>
+                                <div class="flip-card-back">
+                                    <img src="assets/${randNum}-OfHearts.png" style="width:100px;height:200px;">
+                                </div>
+                            </div>
+                        </div>` 
         let cardDiv = document.createElement('div')    
-        cardDiv.innerHTML = inHTML
-        cardDiv.style.width = '100px'
-        cardDiv.style.height = '200px'
+        cardDiv.innerHTML = inHTML  
+        console.log(cardDiv)    
 
         // add event listener to check if queen was found // fix src for diffent host machines
         cardDiv.addEventListener('click', (e) => {          
-            let id = e.target.id
+            let id = cardDiv.childNodes[0].attributes[0].value   
+            cardDiv.childNodes[0].childNodes[1].style.transform = 'rotateY(180deg)'            
+            console.log(cardDiv.childNodes)
             // checks element ID for 'queen'
             if (id == 'queen'){
                 console.log('You Found The QUEEN!!!')
@@ -63,9 +74,17 @@ const makeCards = (numOfCards) => {
 
     // randomly appends queen card 
     let rand = Math.floor(Math.random() * mainDiv.children.length)
-    mainDiv.children[rand].innerHTML = `<img id="queen"src="assets/cardBackSide.png"/>`
-    mainDiv.children[rand].setAttribute('id', 'queen')
-    console.log(mainDiv.children[rand])
+    mainDiv.children[rand].innerHTML = `<div id='queen'class="flip-card">
+                                            <div id='flip-card-inner-queen'class="flip-card-inner">
+                                                <div class="flip-card-front">
+                                                    <img src="assets/cardBackSide.png" style="width:100px;height:200px;">
+                                                </div>
+                                                <div class="flip-card-back">
+                                                    <img src="assets/queenOfHearts.png" style="width:100px;height:200px;">
+                                                </div>
+                                            </div>
+                                        </div>`    
+   
 }
 
 startButton.addEventListener('click', ()=>{
@@ -81,4 +100,5 @@ resetButton.addEventListener('click', ()=>{
 
 // card front side images from  https://upload.wikimedia.org/wikipedia/commons/5/55/Atlasnye_playing_cards_deck.svg
 // card backside from https://www.pinterest.com/pin/105130972527979556/
+// html && css flip animation https://www.w3schools.com/howto/howto_css_flip_card.asp
 // computers are like air conditioning, as soon as you open up windows it becomes useless 
